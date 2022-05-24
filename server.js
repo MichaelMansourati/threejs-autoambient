3260728107950f3339bd84d653c772f4663063bf
Michael Mansourati <mmansourati@gmail.com>
2022-05-23 21:36:35 -0400

    implement osc listener

--- server.js
+++ server.js
@@ -1,17 +1,47 @@
-const express = require('express');
-const path = require('path');
+const osc = require("osc");
+const path = require('path')
+const { WebSocketServer } = require("ws")
 
+const express = require('express')
 const app = express();
-const port = process.env.PORT || 8080;
 
-app.use("/css/", express.static('css'));
-app.use("/js/", express.static('js'));
-app.use("/node_modules/", express.static('node_modules'));
+const http = require('http')
+const server = http.createServer(app);
+
+const { Server } = require('socket.io')
+const io = new Server(server)
+
+const port = 8080;
+const wsPort = 8081
+
+app.use("/", express.static("dist"))
+
+const wss = new WebSocketServer({ port: wsPort });
+
+wss.on('connection', function connection(ws) {
+    let udpPort;
+    console.log("creating websocket");
+    udpPort = new osc.UDPPort({
+        localAddress: "localhost",
+        localPort: wsPort,
+        metadata: true
+    })
+    udpPort.on("message", (msg) => {
+        console.log("message to udp:",  msg)
+        ws.send(JSON.stringify(msg))
+    })
+    udpPort.open()
+});
+
+io.listen(8082)
+
 
 // sendFile will go here
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, './index.html'));
+    res.sendFile(path.join(__dirname, './dist/index.html'));
 });
 
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
+
+server.listen(port, () => {
+    console.log('Server started at http://localhost:' + port);
+});
